f2b85dfe1cd0d30126767f32f66c4b6a86881a2b
msobczyk-x <m.sobczyk55@wp.pl>
2025-02-14 11:22:56 +0100

    Rewrite to tsx

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      // Place to specify ESLint rules
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
